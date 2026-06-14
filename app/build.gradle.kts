b9769a6272f6075aefa5947da78eb5c71eb0bea8
MMMMatt <59835788+MMMMatt@users.noreply.github.com>
2026-06-14 18:52:07 +0800

    Prepare release v1.2.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.hyperosfix.browser"
         minSdk = 34
         targetSdk = 35
-        versionCode = 13
-        versionName = "1.2.3"
+        versionCode = 14
+        versionName = "1.2.4"
     }
 
     signingConfigs {
